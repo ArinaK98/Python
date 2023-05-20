e4468bade99df74781f8749e08b34110298fbe28
KumalyakovaAS <explay1355@mail.ru>
2023-05-20 21:05:46 +0300

    семинар1

---
+++ s1.py
@@ -0,0 +1,44 @@
+# Задача 1
+# Дни на дорогу (n в день, m всего)
+# n = int(input('введите n '))
+# m = int(input('введите m '))
+# c = (m + n -1)// n
+# print(c)
+
+# n = int(input('введите n '))
+# m = int(input('введите m '))
+# c = (m + n -1)// n
+# print(c)
+
+# import math
+# n = int(input('введите n '))
+# m = int(input('введите m '))
+# print(math.ceil(m/n))
+
+# Задача 3
+# Парты для трех классов
+# a = int(input('введите a '))
+# b = int(input('введите b '))
+# c = int(input('введите c '))
+# result = ((-a//2) + (-b//2) + (-c//2) )
+# print(f'количество парт:{-result}')
+
+# Задача 7
+# Високосный год
+# a = int(input('введите год '))
+# if (a%4==0 and a%100!=0) or (a%400==0):
+#     print('yes')
+# else: print('no')
+
+# a = int(input('введите год '))
+# if (a%4==0 and a%100!=0):
+#     print('yes')
+# elif a%400==0:
+#     print('yes')
+# else: print('no')
+
+# a = int(input('введите год '))
+# print("yes" if (a%4==0 and a%100!=0) or (a%400==0) else "no")
+
+
+# print(not(False))
