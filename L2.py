dee940a42eb9f51dea49cabb80851de1d16453e1
KumalyakovaAS <explay1355@mail.ru>
2023-05-29 17:43:46 +0300

    Лекция2

---
+++ L2.py
@@ -0,0 +1,59 @@
+# списки
+# list_1 = [1, 5]
+# print(list_1)
+# list_1.append(8)
+# print(list_1)
+
+# list_1 = []
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1)
+
+
+# кортеж
+# t = ()
+# print(type(t))
+
+# t = (1,)
+# print(type(t))
+
+# v = [1, 8, 9]
+# print(v)
+# print(type(v))
+
+# # v = tuple(v)
+# # print(v)
+# # print(type(v))
+
+
+# a, b, с = v
+# print(a, b, с)
+
+
+# t = (1, 2, 3, 5,)
+# for i in t:
+#     print(i)
+
+# for i in range(len(t)):
+#     print(t[i])
+
+# t[0] = 2
+
+
+
+# словарь
+# d = {}
+# d = dict()
+
+# d['q'] = 'qwerty'
+# print(d)
+
+# d['w'] = 'werty'
+# print(d['q'])
+
+# множества
+
+# a = {1, 8, 6}
+# b = frozenset(a)
+# print(b)
