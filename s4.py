624ff247a00833c02a5451322c9cd0d1b63192eb
KumalyakovaAS <explay1355@mail.ru>
2023-06-05 13:49:50 +0300

    семинар 4

--- s4.py
+++ s4.py
@@ -11,11 +11,12 @@
 
 import random
 
-my_list = input("Введите строку: ")
+my_list = [random.randint(0, 10) for _ in range(20)]
 print(my_list)
 
 counter = {}
 for item in my_list:
     counter[item] = counter.get(item, 0) + 1
+    print(item if counter.get(item) < 2 else str(item) + '_' + str(counter.get(item)))
 
 print(counter)
