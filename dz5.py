60abf1e8a17f8beb421b08eb006322a8d7637a6e
KumalyakovaAS <explay1355@mail.ru>
2023-06-06 19:53:50 +0300

    ДЗ 5

---
+++ dz5.py
@@ -0,0 +1,25 @@
+# Задача 26 степень числа рекурсия
+
+# number = int(input('Введите число: '))
+# power = int(input('Введите кстепень в которую будет возводиться число: '))
+
+# def powering(a , b):
+#     if b == 0:
+#         return 1
+#     else:
+#         return a * powering(a, b-1)
+
+# print(powering(number, power))
+
+# Задача 28
+
+# term1 = int(input('Введите первое слогаемое: '))
+# term2 = int(input('Введите второе слогаемое: '))
+
+# def sum(a , b):
+#     if b == 0:
+#         return a
+#     else:
+#         return sum(a+1, b-1)
+        
+# print(sum(term1, term2))
